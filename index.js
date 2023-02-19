045bbc93ddd33dc37eddace69d325580fb8df02e
unknown <jasmina_t05@outlook.com>
2023-02-19 12:43:54 -0800

    got js to link to html

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const game = document.getElementById('game')
 const scoreDisplay = document.getElementById('score')
 
-const tv = 11
+const general = 20
 const levels = ['easy', 'medium', 'hard']
 
+//column is not working 
 function addGenre(){
-    const column = document.createElement('div')
+    const column = document.createElement("div")
     column.classList.add('genre-column')
-    column.innerHTML = 'this is a genre'
-    game.append(column)
+    column.innerHTML = 'genre goes here'
+    game.appendChild(column)
+    
+//use fetch to generate the questions 
+levels.forEach(level => {
+    const card = document.createElement("div")
+    card.classList.add('card')
+    column.appendChild(card)
+
+
+    fetch(`https://opentdb.com/api.php?amount=20&category=9&difficulty=${level}&type=multiple`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+
+})
+
 }
 
 addGenre()
