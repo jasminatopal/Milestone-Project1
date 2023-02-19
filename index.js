addab241490b14147989137f8e510003586e47ea
unknown <jasmina_t05@outlook.com>
2023-02-19 14:14:17 -0800

    added different genres to game

--- index.js
+++ index.js
@@ -1,30 +1,85 @@
 const game = document.getElementById('game')
 const scoreDisplay = document.getElementById('score')
 
-const general = 20
+const genres = [
+    {
+    name: 'General',
+    id: 9
+},
+
+    {
+    name: 'Books',
+    id: 10
+},
+
+{   
+    name: 'Film',
+    id: 11
+},
+
+{
+    name: 'Music',
+    id: 12
+},
+
+]
 const levels = ['easy', 'medium', 'hard']
 
-//column is not working 
-function addGenre(){
+//create div using js 
+function addGenre(genre) {
     const column = document.createElement("div")
     column.classList.add('genre-column')
-    column.innerHTML = 'genre goes here'
+    column.innerHTML =  genre.name
     game.appendChild(column)
     
-//use fetch to generate the questions 
+
 levels.forEach(level => {
     const card = document.createElement("div")
     card.classList.add('card')
     column.appendChild(card)
 
 
-    fetch(`https://opentdb.com/api.php?amount=20&category=9&difficulty=${level}&type=multiple`)
+//use fetch to generate the questions 
+    fetch(`https://opentdb.com/api.php?amount=20&category=${genre.id}&difficulty=${level}&type=multiple`)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data)
+        //
+        card.setAttribute('data-question', data.results[0].question)
+        card.setAttribute('data-answer', data.results[0].correct_answer)
+        card.setAttribute('data-answer', data.results[0].incorrect_answers)
+        card.setAttribute('data-value', card.getInnerHTML())
+    })
+        card.addEventListener('click', flipCard)
+    })
 
 
-})
+
+//add score values to cards
+if (levels === 'easy') {
+    card.innerHTML = 1
 
 }
 
-addGenre()
+if (levels === 'medium') {
+    card.innerHTML = 3
+}
+
+if (levels === 'hard'){
+    card.innerHTML = 5
+}
+
+
+}
+
+genres.forEach(genre => addGenre(genre))
+
+function flipCard() {
+    console.log('clicked')
+    const textDisplay = document.createElement("div")
+    textDisplay.innerHTML = this.getAttribute('data-question')
+ 
+}
+
+const correctButton = document.createElement('button')
+const incorrectButton = document.createElement('button')
