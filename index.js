8d0ec4c88ab6e3a9fb49ff47f886cb99dc5ac73a
unknown <jasmina_t05@outlook.com>
2023-02-28 20:10:38 -0800

    worked on css and created reset button

--- index.js
+++ index.js
@@ -45,11 +45,13 @@ levels.forEach(level => {
 //add score values to cards 
 if (level === 'easy') {
     card.innerHTML = 100
+    
 
 }
 
 if (level === 'medium') {
     card.innerHTML = 300
+
 }
 
 if (level === 'hard'){
@@ -80,7 +82,7 @@ genres.forEach(genre => addGenre(genre))
 //create context of card including buttons and data from api
 function flipCard() {
     this.innerHTML = ''
-    this.style.fontSize = '20px'
+    this.style.fontSize = '18px'
     const textDisplay = document.createElement("div")
     const correctButton = document.createElement("button")
     const incorrectButton1 = document.createElement("button")
@@ -100,40 +102,63 @@ function flipCard() {
 
     this.append(textDisplay, correctButton, incorrectButton1, incorrectButton2, incorrectButton3)
 
+    //disable event listener to prevent from clicking on multipe questions at once
     const allCards = Array.from(document.querySelectorAll('.card'))
     allCards.forEach(card => card.removeEventListener('click', flipCard))
 
 }
 
 
-
+//funtion to get results when clicking a card 
 function getResult() {
 
+    
     const allCards = Array.from(document.querySelectorAll('.card'))
     allCards.forEach(card => card.addEventListener('click', flipCard))
 
+//parent of the button is the card
     const buttonOnCard = this.parentElement
+//if answer is correct add assigned value to the score
     if (buttonOnCard.getAttribute('data-answer') === this.innerHTML) {
         score = score + parseInt(buttonOnCard.getAttribute('data-value'))
-        scoreDisplay.innerHTML = score
-        buttonOnCard.classList.add('correct-answer')
+        scoreDisplay.innerHTML = score; 
+        buttonOnCard.classList.add('correct-answer') //<-- create classList to be able to change the color of the card in css
+         buttonOnCard.removeEventListener('click', flipCard)
         setTimeout(() => {
             while (buttonOnCard.firstChild) {
                 buttonOnCard.removeChild(buttonOnCard.lastChild)
             }
-            buttonOnCard.innerHTML = buttonOnCard.getAttribute('data-value')
+            buttonOnCard.innerHTML = buttonOnCard.getAttribute('data-value') //<-- shows score received from this card
         }, 100)
+//if answer is incorrect below statement will run 
+
+
 
     } else {(buttonOnCard.classList.add('incorrect-answer'))
+    
         setTimeout(() => {
             while (buttonOnCard.firstChild) {
                 buttonOnCard.removeChild(buttonOnCard.lastChild)
             }
             buttonOnCard.innerHTML = 0
-        }, 100)
-    }
 
+        }, 100)
+
+    
+    }
+    
         buttonOnCard.removeEventListener('click', flipCard)
 
     
 }
+
+//button to reset game
+const resetButton = document.getElementById("reset-button");
+resetButton.addEventListener('click', () => {
+  console.log('reset button clicked');
+});
+
+//unable to get button to work to reset the game - need to work on writing function 
+
+//also need to work on disabling ability to re-click a played card
+
